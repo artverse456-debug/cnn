04917c126a9da7feae66cde9a4c4b44695065bf8
artverse456-debug <artverse456@gmail.com>
2025-11-13 22:34:04 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -70,9 +70,7 @@ export default function LoginPage() {
               required
               placeholder="du@example.com"
               style={{
-               width: 'calc(100% - 8px)',
-
-
+               width: 'calc(100% - 30px)',
                borderRadius: 999,
                 border: '1px solid rgba(148,163,184,0.7)',
                 padding: '9px 14px',
